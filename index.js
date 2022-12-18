7f1f086e8cbe2fa0e1cbb31b590013b5c38865fb
Kennysaruni <kenny.saruni@student.moringaschool.com>
2022-12-18 17:59:27 +0300

    Changed Project

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
+const masterDiv = document.querySelector('div#choose')
+const currSearch = document.querySelector('search-bar')
+const from = document.querySelector('input#from')
+const to = document.querySelector('input#to')
+// currSearch.addEventListener('submit',(e) => {
+//   e.preventDefault()
+//   const fromval = from.value
+//   const toVal = to.value
+// })
+function myFetch(){
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '9875652ae1msh37837c1b985fbc1p1a8f8ajsn2f01cc8bca06',
-		'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+		'X-RapidAPI-Host': 'currency-conversion-and-exchange-rates.p.rapidapi.com'
 	}
 };
 
-fetch('https://spotify81.p.rapidapi.com/top_200_tracks', options)
+fetch('https://currency-conversion-and-exchange-rates.p.rapidapi.com/latest?from=USD&to=EUR%2CGBP', options)
 	.then(response => response.json())
-	.then(response => console.log(response))
-    
-	.catch(err => console.error(err));
-function renderMusic(music){
-    const card = document.createElement('div')
-    card.className = "music-card"
-    const musicList = document.createElement('li')
-    musicList.innerHTML = `
-    <div id="songs">
-    <p>${music.uri}</p>
-    
-    </div>
-    `
-    card.appendChild(musicList)
-}    
+  .then(currency => currencyList(currency))
+	.catch(err => console.error(err));   
+}
+function renderDom(){
+  const myDiv = document.createElement('div')
+  myDiv.className = 'my-div'
+  myDiv.innerHTML =`
+  <div id="search">
+  <form id='search-bar'>
+      <label for="membership">Convert From:</label>
+      <input id='from' type='text' name='search'>
+      <input type='submit' name='submit'/>
+      <label for="membership">Convert to:</label>
+      <input id='to' type='text' name='search'>
+      <input type='submit' name='submit'/>
+    </form>
+  </div> 
+  `
+  masterDiv.appendChild(myDiv)
+}
+renderDom()
+myFetch()
+    masterDiv.addEventListener('click', (e) => console.log(e.target))
